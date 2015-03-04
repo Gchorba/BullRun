59f0dc5435ac49e4f6dee12a1b36a3505c19f44e
Gene Chorba <Gchorba@gmail.com>
2015-03-04 18:16:58 -0500

    commit

--- app/src/main/java/com/matrix/mym/view/fragments/AboutFragment.java
+++ app/src/main/java/com/matrix/mym/view/fragments/AboutFragment.java
@@ -1,9 +1,12 @@
 package com.matrix.mym.view.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.matrix.mym.R;
@@ -22,7 +25,21 @@ public class AboutFragment extends MymMainFragment {
 		View rootView = inflater.inflate(R.layout.fragment_about, container,
 				false);
 		initViews(rootView);
-		return rootView;
+
+
+        Button button = (Button) rootView.findViewById(R.id.button_quiz);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent openBrowser =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.javacodegeeks.com"));
+                startActivity(openBrowser);
+                // do something
+            }
+        });
+        return rootView;
 	}
 
 	private void initViews(View rootView) {
